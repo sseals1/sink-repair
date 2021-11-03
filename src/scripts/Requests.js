eed37003a3ffad50d9066ffbce11e5ed3d9f663d
sseals1 <email@example.com>
2021-11-03 15:05:36 -0500

    Sink repair applications that tracks service requests

---
+++ src/scripts/Requests.js
@@ -0,0 +1,31 @@
+import { getRequests } from "./dataAccess.js"
+
+
+
+
+
+
+export const Requests = () => {
+    const requests = getRequests()
+
+    const convertRequestToListElement = (eachResultOfMap) => {
+        return `
+        <li>
+            ${eachResultOfMap.description}
+            <button class="request__delete"  id="request--${eachResultOfMap.id}">Delete</button>
+        </li>
+    `
+        
+    }
+
+    let html = `
+        <ul>
+            ${requests.map(convertRequestToListElement).join("")}
+        </ul>
+    `
+
+    return html
+    }
+
+
+
---
+++ src/scripts/main.js
@@ -0,0 +1,36 @@
+import { fetchRequests } from "./dataAccess.js"
+import { SinkRepair } from "./SinkRepair.js"
+import { deleteRequest } from "./dataAccess.js"
+
+
+
+
+
+
+const mainContainer = document.querySelector("#container")
+
+const render = () => {
+    fetchRequests().then(
+        () => {
+            mainContainer.innerHTML = SinkRepair()
+        }
+    )
+}
+
+render()
+
+mainContainer.addEventListener(
+    "stateChanged",
+    customEvent => {
+        render()
+    }
+)
+
+
+mainContainer.addEventListener("click", click => {
+    if (click.target.id.startsWith("request--")) {
+        const [,requestId] = click.target.id.split("--")
+        deleteRequest(parseInt(requestId))
+        
+    }
+})
