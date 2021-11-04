b18bd4d3933ed62314806ae4d99486514480a27b
sseals1 <email@example.com>
2021-11-04 09:57:07 -0500

    added commenting to the Delete button click event on the main.js module

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -27,10 +27,13 @@ mainContainer.addEventListener(
 )
 
 
-mainContainer.addEventListener("click", click => {
-    if (click.target.id.startsWith("request--")) {
-        const [,requestId] = click.target.id.split("--")
-        deleteRequest(parseInt(requestId))
+mainContainer.addEventListener(  //listen on mainContainer wich is declared on line 10 and is asigned the value of document.querySelect("#container")
+    "click", // type of event - in this case its a click event
+    click => {  // Function that will do something with the data that will be attached to the click event
+    if (click.target.id.startsWith("request--")) {  // using the .startsWith method to look for the string in the DOM that starts with id of "request"
+        const [,requestId] = click.target.id.split("--") // deconstructing the array using the .split method to remove the 2 dashes from between the word request
+        // and ${eachResultOfMap.id} in the declaration of the creation of the Delete Button in Rquests Module on line 15 and then creating the requestId variable inside of the array
+        deleteRequest(parseInt(requestId)) // invoking the function deleteRequest and passing in as an argument the requestId variable and coverting it back into an integer.
         
     }
 })
